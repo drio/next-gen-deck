d83d54886981938c424a9a525bb70c29d420d3bb
David Rio Deiros <driodeiros@gmail.com>
2012-05-08 12:07:49 -0500

    * Using samtools' flagstats as core for the statistics. Less   dependencies.

--- c/ngd-stats.c
+++ c/ngd-stats.c
@@ -3,219 +3,91 @@
 #include <assert.h>
 #include <string.h>
 #include <limits.h>
-#include <pthread.h>
-#include <zlib.h>
-
-#include "BError.h"
-#include "BLibDefinitions.h"
+#include <unistd.h>
 #include "bam.h"
-#include "sam.h"
-#include "ngd-stats.h"
 
-#define Name "ngd-stats"
-#define ROTATE_NUM 100000
+#define PRG_NAME "ngd-stats"
+
+typedef struct {
+  long long n_reads[2], n_mapped[2], n_pair_all[2], n_pair_map[2], n_pair_good[2];
+  long long n_sgltn[2], n_read1[2], n_read2[2];
+  long long n_dup[2];
+  long long n_diffchr[2], n_diffhigh[2];
+} bam_flagstat_t;
+
+#define flagstat_loop(s, c) do {                    \
+    int w = ((c)->flag & BAM_FQCFAIL) ? 1 : 0;           \
+    ++(s)->n_reads[w];                        \
+    if ((c)->flag & BAM_FPAIRED) {                  \
+      ++(s)->n_pair_all[w];                   \
+      if ((c)->flag & BAM_FPROPER_PAIR) ++(s)->n_pair_good[w];  \
+      if ((c)->flag & BAM_FREAD1) ++(s)->n_read1[w];        \
+      if ((c)->flag & BAM_FREAD2) ++(s)->n_read2[w];        \
+      if (((c)->flag & BAM_FMUNMAP) && !((c)->flag & BAM_FUNMAP)) ++(s)->n_sgltn[w];  \
+      if (!((c)->flag & BAM_FUNMAP) && !((c)->flag & BAM_FMUNMAP)) { \
+        ++(s)->n_pair_map[w];                 \
+        if ((c)->mtid != (c)->tid) {              \
+          ++(s)->n_diffchr[w];                \
+          if ((c)->qual >= 5) ++(s)->n_diffhigh[w];     \
+        }                           \
+      }                             \
+    }                               \
+    if (!((c)->flag & BAM_FUNMAP)) ++(s)->n_mapped[w];        \
+    if ((c)->flag & BAM_FDUP) ++(s)->n_dup[w];            \
+  } while (0)
+
+bam_flagstat_t *bam_flagstat_core(bamFile fp)
+{
+  bam_flagstat_t *s;
+  bam1_t *b;
+  bam1_core_t *c;
+  int ret;
+  s = (bam_flagstat_t*)calloc(1, sizeof(bam_flagstat_t));
+  b = bam_init1();
+  c = &b->core;
+  while ((ret = bam_read1(fp, b)) >= 0)
+    flagstat_loop(s, c);
+  bam_destroy1(b);
+  if (ret != -1)
+    fprintf(stderr, "[bam_flagstat_core] Truncated file? Continue anyway.\n");
+  return s;
+}
 
 int main(int argc, char *argv[])
 {
-  samfile_t *fp_in = NULL;
-  int32_t i;
-  bam1_t *b=NULL;
-  bam_stats_t *stats=NULL;
+  bamFile fp;
+  bam_header_t *header;
+  bam_flagstat_t *s;
 
-  if(3 <= argc) {
-    stats = bam_stats_t_init();
-
-    for(i=2;i<argc;i++) {
-      fprintf(stderr, "%s", BREAK_LINE);
-      fprintf(stderr, "Reading in from %s.\n", argv[i]);
-      fp_in = samopen(argv[i], "rb", 0);
-      if(NULL == fp_in) {
-        PrintError(Name,
-          argv[i],
-          "Could not open file for reading",
-          Exit,
-          OpenFileError);
-      }
-
-      b = bam_init1();
-      int64_t b_ctr = 0;
-      while(0 < samread(fp_in, b)) { // Read one alignment
-        b_ctr++;
-        if(0 == (b_ctr % 100000)) fprintf(stderr, "\r%lld", (long long int)b_ctr);
-
-        // Add
-        bam_stats_t_add(stats, b);
-
-        // Destroy bam
-        bam_destroy1(b);
-        // Reinitialize
-        b = bam_init1();
-      }
-
-      fprintf(stderr, "\r%lld\n", (long long int)b_ctr);
-      bam_destroy1(b);
-      fprintf(stderr, "%s", BREAK_LINE);
-      samclose(fp_in);
-    }
-
-    // Output
-    char seed_name[100] = {0};
-    strcpy(seed_name, argv[1]);
-    for(i=1;i<argc;i++)
-      fprintf(stdout, "%s.\n", argv[i]);
-    bam_stats_t_print(stats, seed_name);
-
-    bam_stats_t_destroy(stats);
-    stats=NULL;
-
-    fprintf(stderr, "%s", BREAK_LINE);
-    fprintf(stderr, "Terminating successfully!\n");
-    fprintf(stderr, "%s", BREAK_LINE);
-  }
-  else {
-    fprintf(stderr, "Usage: %s [OPTIONS]\n", Name);
-    fprintf(stderr, "\t<seed_output_name> <bam file(s)>\n");
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s flagstat <output_seed> <in.bam>\n", PRG_NAME);
+    return 1;
   }
 
-  return 0;
-}
+  char seed_name[50] = {0};
+  strcpy(seed_name, argv[1]);
 
-bam_stats_t *bam_stats_t_init()
-{
-  char *FnName="bam_stats_t_init";
-  bam_stats_t *stats=NULL;
-  int32_t i;
+  fp = strcmp(argv[2], "-") ? bam_open(argv[2], "r") : bam_dopen(fileno(stdin), "r");
+  assert(fp);
+  header = bam_header_read(fp);
+  s = bam_flagstat_core(fp);
 
-  stats=malloc(sizeof(bam_stats_t));
-  if(NULL == stats)
-    PrintError(FnName, "stats", "Could not allocate memory", Exit, MallocMemory);
+  printf("key,value\n");
+  printf("total_n_reads,%lld\n", s->n_reads[0]);
+  printf("total_n_failed_reads,%lld\n", s->n_reads[1]);
+  printf("total_n_duplicates,%lld\n", s->n_dup[0]);
+  printf("total_n_mapped_reads,%lld\n", s->n_mapped[0]);
+  printf("total_n_paired_reads,%lld\n", s->n_pair_all[0]);
+  printf("total_n_read1,%lld\n", s->n_read1[0]);
+  printf("total_n_read2,%lld\n", s->n_read2[0]);
+  //printf("%lld + %lld properly paired (%.2f%%:%.2f%%)\n", s->n_pair_good[0], s->n_pair_good[1], (float)s->n_pair_good[0] / s->n_pair_all[0] * 100.0, (float)s->n_pair_good[1] / s->n_pair_all[1] * 100.0);
+  printf("total_n_both_ends_mapped,%lld\n", s->n_pair_map[0]);
+  printf("total_n_pairs_only_one_end_mapped,%lld\n", s->n_sgltn[0]);
+  printf("total_n_pairs_with_mate_mapping_to_diff_chrms,%lld\n", s->n_diffchr[0]);
+  //printf("%lld + %lld with mate mapped to a different chr (mapQ>=5)\n", s->n_diffhigh[0], s->n_diffhigh[1]);
 
-  stats->num_raw_paired_reads = 0;
-  stats->num_raw_single_end_reads = 0;
-  stats->num_paired_reads = 0;
-  stats->num_unpaired_reads = 0;
-  stats->num_single_end_reads = 0;
-  stats->num_paired_reads_pcr_dup = 0;
-  stats->num_unpaired_reads_pcr_dup = 0;
-  stats->num_single_end_reads_pcr_dup = 0;
-  for(i=0;i<256;i++)
-    stats->num_at_quality_score[i]=0;
-  for(i=-1;i<MAX_INSERT_SIZE;i++)
-    stats->num_at_insert_size[i]=0;
-  return stats;
-}
-
-void bam_stats_t_add(bam_stats_t *stats, bam1_t *b)
-{
-  int64_t absolute_is;
-
-  if(b->core.flag & BAM_FPAIRED) { // paired end
-    if(b->core.flag & BAM_FREAD1) { // first read
-      stats->num_raw_paired_reads++;
-      if(!(b->core.flag & BAM_FUNMAP) && !(b->core.flag & BAM_FMUNMAP)) {
-        // both ends mapped
-        stats->num_paired_reads++;
-        if(b->core.flag & BAM_FDUP) stats->num_paired_reads_pcr_dup++;
-        else { // No duplicate, first read, both ends map
-          absolute_is = b->core.isize < 0 ? b->core.isize * -1 : b->core.isize;
-          if (absolute_is < MAX_INSERT_SIZE)
-            stats->num_at_insert_size[absolute_is]++;
-          else // Aberrant insert size
-            stats->num_at_insert_size[-1]++;
-        }
-      }
-      else if(!(b->core.flag & BAM_FUNMAP) && (b->core.flag & BAM_FMUNMAP)) {
-        // this end mapped, mate unmapped
-        stats->num_unpaired_reads++;
-        if(b->core.flag & BAM_FDUP) stats->num_unpaired_reads_pcr_dup++;
-      }
-      else if((b->core.flag & BAM_FUNMAP) && !(b->core.flag & BAM_FMUNMAP)) {
-        // this end unmapped, mate mapped
-        stats->num_unpaired_reads++;
-        if(b->core.flag & BAM_FDUP) stats->num_unpaired_reads_pcr_dup++;
-      }
-    }
-  }
-  else { // single end
-    stats->num_raw_single_end_reads++;
-    if(!(b->core.flag & BAM_FUNMAP)) { // mapped
-      stats->num_single_end_reads++;
-      if(b->core.flag & BAM_FDUP) stats->num_single_end_reads_pcr_dup++;
-    }
-  }
-  // Update mapq
-  stats->num_at_quality_score[b->core.qual]++; // this exploits the fact that b->core.qual is an unsigned 8-bit integer.
-}
-
-void bam_stats_t_print(bam_stats_t *stats, char *seed)
-{
-  FILE *fp;
-  char fn_stats[100] = {0};
-
-  /* First, the basic key value pairs */
-  strcpy(fn_stats, seed);
-  strcat(fn_stats, ".stats.csv");
-  if ((fp = fopen(fn_stats, "w")) == NULL) {
-    fprintf(stderr, "Problems writing to file %s.", fn_stats);
-    exit(1);
-  }
-
-  fprintf(fp, "key,value\n"); // CSV header
-  fprintf(fp, "Number of raw paired reads,%lld\n",
-      (long long int)stats->num_raw_paired_reads);
-  fprintf(fp, "Number of raw single end reads,%lld\n",
-      (long long int)stats->num_raw_single_end_reads);
-  fprintf(fp, "Number of mapped paired reads,%lld\n",
-      (long long int)stats->num_paired_reads);
-  fprintf(fp, "Number of mapped unpaired reads,%lld\n",
-      (long long int)stats->num_unpaired_reads);
-  fprintf(fp, "Number of mapped single end reads,%lld\n",
-      (long long int)stats->num_single_end_reads);
-  fprintf(fp, "Number of mapped paired reads pcr duplicates,%lld\n",
-      (long long int)stats->num_paired_reads_pcr_dup);
-  fprintf(fp, "Number of mapped unpaired reads pcr duplicates,%lld\n",
-      (long long int)stats->num_unpaired_reads_pcr_dup);
-  fprintf(fp, "Number of mapped single end reads pcr duplicates,%lld\n",
-      (long long int)stats->num_single_end_reads_pcr_dup);
-  fclose(fp);
-
-  /* Now the mapq distribution key value pairs. */
-  int32_t i;
-  char fn_mapq[100] = {0};
-
-  /* First, the basic key value pairs */
-  strcpy(fn_mapq, seed);
-  strcat(fn_mapq, ".mapq.dist.csv");
-  if ((fp = fopen(fn_mapq, "w")) == NULL) {
-    fprintf(stderr, "Problems writing to file %s.", fn_mapq);
-    exit(1);
-  }
-
-  fprintf(fp, "amount,mapping_quality\n");
-  for(i=0;i<256;i++)
-    fprintf(fp, "%lld,%d\n", (long long int)stats->num_at_quality_score[i], i);
-  fclose(fp);
-
-  /* Now, insert size distribution stats */
-  char fn_is[100] = {0};
-
-  /* Insert size distro data  */
-  strcpy(fn_is, seed);
-  strcat(fn_is, ".isize.dist.csv");
-  if ((fp = fopen(fn_is, "w")) == NULL) {
-    fprintf(stderr, "Problems writing to file %s.", fn_is);
-    exit(1);
-  }
-
-  fprintf(fp, "amount,insert_size\n");
-  for(i=-1;i<MAX_INSERT_SIZE;i++)
-    fprintf(fp, "%lld,%d\n", (long long int)stats->num_at_insert_size[i], i);
-
-  fclose(fp);
-}
-
-void bam_stats_t_destroy(bam_stats_t *s)
-{
   free(s);
-  s=NULL;
+  bam_header_destroy(header);
+  bam_close(fp);
+  return 0;
 }
