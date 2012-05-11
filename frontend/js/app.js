582820a4c8134242cd3868bd4bb478f29e1a6619
David Rio Deiros <driodeiros@gmail.com>
2012-05-11 16:02:22 -0500

    * Adding dotplot

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,7 +1,26 @@
 /* Load the app when the DOM is ready. Thanks Jquery. */
 $(function(){
   $.getJSON('http://localhost:7379/get/stats', function(data){
-    var o = $.parseJSON(data.get);
-    console.log(o["rhesus-23_samples_endometriosis-1"]["per_dups"]);
+    var bams       = $.parseJSON(data.get),
+        a_per_dups = [];
+
+    _.each(_.values(bams), function(e, i){
+      a_per_dups.push([i, e["per_dups"]]);
+    });
+
+    //console.log(bams["rhesus-23_samples_endometriosis-1"]["per_dups"]);
+    console.log(a_per_dups);
+
+    var dataset = [];
+    var numDataPoints = 100;
+    var yRange = Math.random() * 1000;
+    for (var i = 0; i < numDataPoints; i++) {
+      var newNumber1 = i;
+      var newNumber2 = Math.round(Math.random() * yRange);
+      dataset.push([newNumber1, newNumber2]);
+    }
+
+    plots.barplot("#main-plot", a_per_dups);
+    //plots.barplot("#main-plot", dataset);
   });
 });
---
+++ frontend/js/plots.js
@@ -0,0 +1,65 @@
+var plots = (function() {
+  var plots = {};
+
+  plots.barplot = function(sel, data) {
+    var w       = 800,
+        h       = 400,
+        padding = 20,
+        radio   = 3;
+
+    var x = d3.scale.linear()
+              .domain([0, d3.max(data, function(d) { return d[0];})])
+              .range([padding, w - padding * 2]);
+
+    var y = d3.scale.linear()
+              .domain([0, d3.max(data, function(d) { return d[1];})])
+              .range([h - padding, padding]);
+
+    var x_axis = d3.svg.axis()
+                   .scale(x)
+                   .orient("bottom");
+
+    var y_axis = d3.svg.axis()
+                   .scale(y)
+                   .orient("left");
+
+    var svg = d3.select(sel)
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+    svg.selectAll("circle")
+       .data(data)
+       .enter()
+       .append("circle")
+       .attr("cx", function(d) { return x(d[0]); })
+       .attr("cy", function(d) { return y(d[1]); })
+       .attr("r", radio);
+
+    /*
+    svg.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .text(function(d)       { return d[0] + "," + d[1]; })
+        .attr("x", function(d)  { return x(d[0]); })
+        .attr("y", function(d)  { return y(d[1]); })
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "red");
+    */
+
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + (h - padding) + ")")
+        .call(x_axis);
+
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + padding + ",0)")
+        .call(y_axis);
+
+  };
+
+  return plots;
+})();
