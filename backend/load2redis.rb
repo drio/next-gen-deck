9b159698b17cc0e4366a0de7d9bfe0466e994cf1
David Rio Deiros <driodeiros@gmail.com>
2012-05-11 10:55:35 -0500

    * Calculate percentage instead of dumping the actual num of reads

--- backend/load2redis.rb
+++ backend/load2redis.rb
@@ -13,11 +13,13 @@ require 'pp'
 require 'redis'
 require 'json'
 
+def per(total, n)
+  ((n.to_f*100)/total.to_f).round(2)
+end
+
 # Dump into redis the data we just loaded, do it
 # in such a way that later we can query redis
 # via web and answer these type of questions:
-#
-# Question that will be asked from the frontend
 #
 # 1. The [ %_mapped || %_dups ] for all the bams
 #   redis: per_mapped[bam_full_path] = %_of_mapped
@@ -29,33 +31,34 @@ require 'json'
 # TODO: This needs heavy refactoring ... looks uggly
 def dump_in_redis(h)
   redis = Redis.new
-  h.each do |levels, h_csvs|
-    h_csvs.each do |csv, a_data|
-      #puts "levels: #{levels}"; puts "file: #{csv}"; o = {:header => "", :data => ""}
+  h.each do |levels, h_csvs|              # a directory
+    h_csvs.each do |csv, a_data|          # csvs on that directory
       dist_is, dist_mq_r1, dist_mq_r2 = [{}, {}, {}]
-      a_data.each_with_index do |line, i|
-        #o[:header] = line if i == 0
-        #o[:data]   = " " + line.to_s if i != 0
-
+      n_reads = nil
+      a_data.each_with_index do |line, i| # a line from a csv
         # If we are processing a csv stats and we are in the metric we
         # are interested on, dump it to redis
         if csv =~ /stats/
-          redis.hset("per_dups", levels, line[1]) if line[0] == "n_duplicate_reads"
-          redis.hset("per_mapped", levels, line[1]) if line[0] == "n_reads_mapped"
+          n_reads = line[1] if line[0] == "n_reads"
+          if line[0] == "n_duplicate_reads"
+            redis.hset("per_dups", levels, per(n_reads,line[1]))
+          end
+          if line[0] == "n_reads_mapped"
+            redis.hset("per_mapped", levels, per(n_reads, line[1]))
+          end
         end
 
         # Keep the dist values for the different distributions for later
-        dist_is[line[0].to_i] = line[1].to_i if csv =~ /isize/ && i != 0
+        dist_is[line[0].to_i]    = line[1].to_i if csv =~ /isize/ && i != 0
         dist_mq_r1[line[0].to_i] = line[1].to_i if csv =~ /r1\.mapq/  && i != 0
         dist_mq_r2[line[0].to_i] = line[1].to_i if csv =~ /r2\.mapq/  && i != 0
       end
 
       # if we have distribution data, dump it in redis as a JSON object
       clean_level = levels.gsub(/\s/, '-')
-      redis.set "is-"    + clean_level, dist_is.to_json unless dist_is.empty?
+      redis.set "is-"    + clean_level, dist_is.to_json    unless dist_is.empty?
       redis.set "mq-r1-" + clean_level, dist_mq_r1.to_json unless dist_mq_r1.empty?
       redis.set "mq-r2-" + clean_level, dist_mq_r2.to_json unless dist_mq_r2.empty?
-      #puts "header: #{o[:header]}"; puts "data: #{o[:data]}"
     end
   end
 end
@@ -83,6 +86,3 @@ Find.find(".") do |f|
 end
 $stderr.puts ">> Dumping into redis ..."
 dump_in_redis csvs
-
-
-
