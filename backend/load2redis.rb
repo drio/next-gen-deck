8db544c4d5da37933b318e467f4b54852c5c96e4
David Rio Deiros <driodeiros@gmail.com>
2012-05-19 12:16:00 -0500

    * Heavy refactoring of ruby tool

--- backend/load2redis.rb
+++ backend/load2redis.rb
@@ -7,29 +7,7 @@
 #
 # All deps (except redis) come with ruby
 #
-require 'find'
-require 'csv'
-require 'pp'
-require 'redis'
-require 'json'
-
-def per(total, n)
-  ((n.to_f*100)/total.to_f).round(2)
-end
-
-# The value of the keys stored in redis have to have
-# to attributes, one is the type and the other the data (json data)
-# The type will be use in the client to determine what type
-# of data we have in the object
-def tj(type, data)
-  if type == "isize"
-    data[-1] = 0;
-    reduced_data = {};
-    data.each {|k,v| reduced_data[k] = v if k <= 1000}
-    data = reduced_data
-  end
-  {"type" => type, "data" => data}.to_json
-end
+%w{find csv pp redis json ostruct}.each {|l| require l}
 
 # Dump into redis the data we just loaded, do it
 # in such a way that later we can query redis
@@ -42,47 +20,82 @@ end
 # 2. All the isizes/amount pairs for a particular bam
 #   redis: stats[full_path] = val
 #
-# TODO: This needs heavy refactoring ... looks uggly
-def dump_in_redis(h)
-  redis = Redis.new
-  all   = {} # This will hold all the bams and its stats
-  h.each do |levels, h_csvs|              # a directory
-    h_csvs.each do |csv, a_data|          # csvs on that directory
-      dist_is, dist_mq_r1, dist_mq_r2, stats = [{}, {}, {}, {}]
-      n_reads = nil
-      a_data.each_with_index do |line, i| # a line from a csv
-        # If we are processing a csv stats and we are in the metric we
-        # are interested on, dump it to redis
-        if csv =~ /stats/
-          n_reads = line[1] if line[0] == "n_reads"
-          if line[0] == "n_duplicate_reads"
-            #redis.hset("per_dups", levels, per(n_reads,line[1]))
-            stats["per_dups"] = per n_reads, line[1]
-          elsif line[0] == "n_reads_mapped"
-            #redis.hset("per_mapped", levels, per(n_reads, line[1]))
-            stats["per_mapped"] = per n_reads, line[1]
+module ToRedis
+  REDIS = Redis.new
+
+  def ToRedis.dump(hash_with_all_the_csvs)
+    h    = hash_with_all_the_csvs
+    @all = {} # we'll keep here the stats for all the bams
+    h.each do |levels, h_csvs|              # a directory
+      h_csvs.each do |csv, a_data|          # csvs on that directory
+        @dists = prepare_dist_struct        # hashes storing the dists per bam
+        n_reads = nil                       # number of reads in a bam
+        a_data.each_with_index do |line, i| # a line from a csv
+          if line[0] == "n_reads"
+            n_reads = line[1]
           else
-            stats[line[0]] = line[1]
+            process_line csv, line, i, n_reads
           end
         end
-        # Keep the dist values for the different distributions for later
-        dist_is[line[0].to_i]    = line[1].to_i if csv =~ /isize/ && i != 0
-        dist_mq_r1[line[0].to_i] = line[1].to_i if csv =~ /r1\.mapq/  && i != 0
-        dist_mq_r2[line[0].to_i] = line[1].to_i if csv =~ /r2\.mapq/  && i != 0
+        send_dists_to_redis levels
       end
-
-      clean_level = levels.gsub(/\s/, '-')
-      # Save the stats so we can dump to redis later
-      all[clean_level] = stats unless stats.empty?
-      # if we have distribution data, dump it in redis as a JSON object
-      redis.set "is-"    + clean_level, tj("isize", dist_is)    unless dist_is.empty?
-      redis.set "mq-r1-" + clean_level, tj("mq-r1", dist_mq_r1) unless dist_mq_r1.empty?
-      redis.set "mq-r2-" + clean_level, tj("mq-r2", dist_mq_r2) unless dist_mq_r2.empty?
     end
+    # Dump all the events that we have and its stats
+    REDIS.set "stats", @all.to_json
   end
-  # Dump all the events that we have and its stats
-  # You can retrieve these data via http with: GET /get/stats
-  all.each {|id, json| redis.set "stats", all.to_json}
+
+  def ToRedis.process_line(csv, line, i, n_reads)
+    d = @dists; l = line;
+    if csv =~ /stats/
+      if line[0] == "n_duplicate_reads" # Calculate %, and save it
+        d.stats["per_dups"]   = per n_reads, l[1]
+      elsif l[0] == "n_reads_mapped"    # Calculate %, and save it
+        d.stats["per_mapped"] = per n_reads, l[1]
+      else                              # Regular stat, save it
+        d.stats[line[0]] = line[1]
+      end
+    end
+    # Keep the dist values for the different distributions for later
+    d.is[l[0].to_i]    = l[1].to_i if csv =~ /isize/     && i != 0
+    d.mq_r1[l[0].to_i] = l[1].to_i if csv =~ /r1\.mapq/  && i != 0
+    d.mq_r2[l[0].to_i] = l[1].to_i if csv =~ /r2\.mapq/  && i != 0
+  end
+
+  def ToRedis.per(total, n)
+    ((n.to_f*100)/total.to_f).round(2)
+  end
+
+  def ToRedis.prepare_dist_struct
+    os = OpenStruct.new
+    os.is = {}; os.mq_r1 = {}; os.mq_r2 = {}; os.stats = {}
+    os
+  end
+
+  def ToRedis.send_dists_to_redis(levels)
+    r = REDIS; d = @dists
+    clean_level = levels.gsub(/\s/, '-')
+    # Save the stats so we can dump to redis later
+    @all[clean_level] = d.stats unless d.stats.empty?
+
+    # if we have distribution data, dump it in redis as a JSON object
+    r.set "is-"    + clean_level, tj("isize", d.is)    unless d.is.empty?
+    r.set "mq-r1-" + clean_level, tj("mq-r1", d.mq_r1) unless d.mq_r1.empty?
+    r.set "mq-r2-" + clean_level, tj("mq-r2", d.mq_r2) unless d.mq_r2.empty?
+  end
+
+  # The value of the keys stored in redis have to have
+  # to attributes, one is the type and the other the data (json data)
+  # The type will be use in the client to determine what type
+  # of data we have in the object
+  def ToRedis.tj(type, data)
+    if type == "isize"
+      data[-1] = 0;
+      reduced_data = {};
+      data.each {|k,v| reduced_data[k] = v if k <= 1000}
+      data = reduced_data
+    end
+    {"type" => type, "data" => data}.to_json
+  end
 end
 
 #
@@ -104,4 +117,5 @@ $stderr.puts ">> Loading csvs ..."
 csvs = Hash.new {|h, k| h[k] = {}}
 Find.find(".") {|f| csvs = load_data csvs, f if f =~ /\.csv$/ }
 $stderr.puts ">> Dumping into redis ..."
-dump_in_redis csvs
+#dump_in_redis csvs
+ToRedis.dump csvs
